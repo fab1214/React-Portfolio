e1476128fdfc4e43f0fa8d687c177264ee2328a0
fab1214 <88295561+fab1214@users.noreply.github.com>
2022-01-06 23:43:11 -0500

    add nav and about skeleton components

---
+++ src/components/Nav/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Nav = () => {
+    return (
+        <header>
+            <a href="#About">About</a> 
+            <a href="#Portfolio">Portfolio</a>
+            <a href="#Contact">Contact</a>
+            <a href="#Resume">Resume</a>
+        </header>
+    )
+}
+
+export default Nav;
