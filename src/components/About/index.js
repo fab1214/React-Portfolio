14c7dea2ffe2f94acbf027314b0875c0e6c80746
fab1214 <88295561+fab1214@users.noreply.github.com>
2022-01-15 22:07:27 -0500

    style about more

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -7,39 +7,34 @@ const About = () => {
   return (
     <section>
     <div className="container">
-      <div className="row">
-        <div className="typed-text col-12">
+
+      <div className="row justify-content-space-between">
+
+        <div className="typed-text col-6">
           <Typed strings={['Welcome to my page!']}
-          typeSpeed={40}
-          backSpeed={50}
+          typeSpeed={70}
+          backSpeed={60}
           loop
           ></Typed>
-          </div>
+          <p className='about-me'>
+          My name is Fabricio Bustamante and I am an incoming Full Stack Web
+          Developer from the Rutgers Coding Bootcamp. I reside in the NJ area and
+          am actively open to new job opportunities where I am able to exemplify
+          my new skills in web development, as well as continuting to learn in the
+          field. I hope any guests who visit my page enjoy some of the projects I
+          have worked on throughout my bootcamp experience!
+
+          Feel free to leave a note as well in the{" "}
+          <Link to="/contact">Contact</Link> section!{" "}
+          </p>
+        </div>
+
+        <div className='col-6'>
+          <img src={photo} width="300" height="auto" />
+        </div>
       </div>
-      <div className="row justify-content-space-around">
-      <div className="col-6">
-      <p>
-        My name is Fabricio Bustamante and I am an incoming Full Stack Web
-        Developer from the Rutgers Coding Bootcamp. I reside in the NJ area and
-        am actively open to new job opportunities where I am able to exemplify
-        my new skills in web development, as well as continuting to learn in the
-        field. I hope any guests who visit my page enjoy some of the projects I
-        have worked on throughout my bootcamp experience!
-      </p>
-      <p>
-        Feel free to leave a note as well in the{" "}
-        <Link to="/contact">Contact</Link> section :){" "}
-      </p>
-      <p>
-        Enjoy!
-        <br />- Fabricio
-      </p>
+
       </div>
-      <div className='col-6'>
-        <img src={photo} width="400" height="auto" />
-      </div>
-    </div>
-    </div>
     </section>
   );
 };
